fb7921d5865bb32ae18789634e01fcbdd4ee01e2
modestick <modestick@proton.me>
2025-12-11 17:25:23 +0200

    Add simple PHP app 88360

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>88360</title>
+</head>
+<body>
+    <h1>Hello World! 👋</h1>
+    <p>Title of this app: 88360</p>
+    <p>This is a simple PHP app ready to deploy.</p>
+</body>
+</html>
